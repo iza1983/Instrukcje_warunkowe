7afaaefe857cdad059ce6b9994e81cf35b6bd75c
Iza M <imalnowicz@gmail.com>
2017-03-11 09:40:42 +0100

    poprawione

---
+++ README.md
@@ -0,0 +1,1 @@
+# kodilla-8.5 - instrukcje warunkowe if, switch, short if
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 var a = prompt('Wpisz wartość a'),
     b = prompt('Wpisz wartość b'),
-	value = ((a*a) + (2 * a * b) - (b*b));
+    value = ((a * a) + (2 * a * b) - (b * b));
 
-alert('Wynik działania ' + ((a*a) + (2 * a * b) - (b*b)));	
+alert('Wynik działania ' + ((a * a) + (2 * a * b) - (b * b)));
 console.log('Wartość a: ' + a + ' Wartość b: ' + b + ' Wynik: ' + value);
 
 if (value > 0) {
-   console.log('Wynik dodatni');
+   console.log('Wynik jest dodatni');
 } else if (value < 0) {
-	console.log('Wynik ujemny');
+   console.log('Wynik jest ujemny');
 } else {
-	console.log('Wynik jest równy 0');
+   console.log('Wynik jest równy 0');
 }
 
 var valueScore = (value == 0) ? 'Wynik działania jest równy 0' : 'Wynik działania jest różny od 0';
-    console.log(valueScore);
+console.log(valueScore);
