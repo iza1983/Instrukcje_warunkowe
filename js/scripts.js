20be40c6567e8ba1349bcd5dbe5db0fa06940284
iza1983 <imalnowicz@gmail.com>
2017-03-13 12:22:28 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,6 @@ var a = prompt('Wpisz wartość a'),
     b = prompt('Wpisz wartość b'),
     value = ((a * a) + (2 * a * b) - (b * b));
 
-alert('Wynik działania ' + ((a * a) + (2 * a * b) - (b * b)));
+alert('Wynik działania ' + value);
 console.log('Wartość a: ' + a + ' Wartość b: ' + b + ' Wynik: ' + value);
-
-if (value > 0) {
-   console.log('Wynik jest dodatni');
-} else if (value < 0) {
-   console.log('Wynik jest ujemny');
-} else {
-   console.log('Wynik jest równy 0');
-}
-
-var valueScore = (value == 0) ? 'Wynik działania jest równy 0' : 'Wynik działania jest różny od 0';
-console.log(valueScore);
+console.log(value === 0 ? 'Wynik równy 0' : (value > 0 ? 'Wynik dodatni' : 'Wynik ujemny'));
